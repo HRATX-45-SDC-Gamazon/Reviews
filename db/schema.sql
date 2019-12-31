47e97f61e6e51a07c9751abae3bd9c29daf8fecb
Ish Tahir <tahirismaeel@gmail.com>
2019-12-31 12:20:13 -0600

    Updated schema file for only reviews

--- db/schema.sql
+++ db/schema.sql
@@ -4,50 +4,21 @@ CREATE DATABASE sdcreviews;
 
 USE sdcreviews;
 
-CREATE TABLE products
+CREATE TABLE reviews
 (
-    id INT PRIMARY KEY NOT NULL
-    AUTO_INCREMENT,
-    name TEXT NOT NULL,
-    category TEXT NOT NULL,
-    average INT NOT NULL
+    productId INT NOT NULL PRIMARY KEY,
+    person TEXT NOT NULL,
+    personAvatar TEXT NOT NULL,
+    title TEXT NOT NULL,
+    body TEXT NOT NULL,
+    rating INT NOT NULL,
+    helpfulCount INT NOT NULL,
+    date TEXT NOT NULL,
+    verified TINYINT NOT NULL
 );
 
-    CREATE TABLE comments
-    (
-        productId INT NOT NULL,
-        person TEXT NOT NULL,
-        personAvatar TEXT NOT NULL,
-        title TEXT NOT NULL,
-        body TEXT NOT NULL,
-        rating INT NOT NULL,
-        helpfulCount INT NOT NULL,
-        date TEXT NOT NULL,
-        verified TINYINT NOT NULL,
-        id INT PRIMARY KEY NOT NULL
-        AUTO_INCREMENT
-);
-
-LOAD DATA LOCAL INFILE '/Users/ishhr/Desktop/featureReviews/products.csv' 
-INTO TABLE products 
-FIELDS TERMINATED BY ',' 
-ENCLOSED BY '"'
-LINES TERMINATED BY '\n';
-
-LOAD DATA LOCAL INFILE '/Users/ishhr/Desktop/featureReviews/products2.csv' 
-INTO TABLE products 
-FIELDS TERMINATED BY ',' 
-ENCLOSED BY '"'
-LINES TERMINATED BY '\n';
-
-LOAD DATA LOCAL INFILE '/Users/ishhr/Desktop/featureReviews/comments.csv' 
-INTO TABLE comments 
-FIELDS TERMINATED BY ',' 
-ENCLOSED BY '"'
-LINES TERMINATED BY '\n';
-
-LOAD DATA LOCAL INFILE '/Users/ishhr/Desktop/featureReviews/comments2.csv' 
-INTO TABLE comments 
+LOAD DATA LOCAL INFILE '/Users/ishhr/Desktop/featureReviews/reviewssql.csv' 
+INTO TABLE reviews 
 FIELDS TERMINATED BY ',' 
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n';
