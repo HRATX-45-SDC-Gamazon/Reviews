dbbc648a979b47eca92567d8b169b04942a0d7c7
Ish Tahir <tahirismaeel@gmail.com>
2019-12-31 12:19:16 -0600

    Updated gitignore file

--- db/schema.sql
+++ db/schema.sql
@@ -6,37 +6,48 @@ USE sdcreviews;
 
 CREATE TABLE products
 (
-    id INT PRIMARY KEY UNIQUE,
+    id INT PRIMARY KEY NOT NULL
+    AUTO_INCREMENT,
     name TEXT NOT NULL,
     category TEXT NOT NULL,
-    comments TEXT NOT NULL,
-    categoryRatings TEXT NOT NULL,
-    average INT NOT NULL,
-    totalPictures TEXT NOT NULL,
-    individualRatings TEXT NOT NULL,
+    average INT NOT NULL
 );
 
-CREATE TABLE comments
-(
-    id INT PRIMARY KEY AUTO_INCREMENT UNIQUE,
-    productId INT NOT NULL,
-    person TEXT NOT NULL,
-    personAvatar TEXT NOT NULL,
-    title TEXT NOT NULL,
-    body TEXT NOT NULL,
-    rating INT NOT NULL,
-    helpfulCount INT NOT NULL,
-    date TEXT NOT NULL,
-    verified TINYINT NOT NULL
+    CREATE TABLE comments
+    (
+        productId INT NOT NULL,
+        person TEXT NOT NULL,
+        personAvatar TEXT NOT NULL,
+        title TEXT NOT NULL,
+        body TEXT NOT NULL,
+        rating INT NOT NULL,
+        helpfulCount INT NOT NULL,
+        date TEXT NOT NULL,
+        verified TINYINT NOT NULL,
+        id INT PRIMARY KEY NOT NULL
+        AUTO_INCREMENT
 );
 
-CREATE TABLE individualRatings
-(
-    id INT
-    AUTO_INCREMENT PRIMARY KEY NOT NULL,
-    oneStarRatings TINYINT NOT NULL,
-    twoStarRatings TINYINT NOT NULL,
-    threeStarRatings TINYINT NOT NULL,
-    fourStarRatings TINYINT NOT NULL,
-    fiveStarRatings TINYINT NOT NULL
-)
+LOAD DATA LOCAL INFILE '/Users/ishhr/Desktop/featureReviews/products.csv' 
+INTO TABLE products 
+FIELDS TERMINATED BY ',' 
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n';
+
+LOAD DATA LOCAL INFILE '/Users/ishhr/Desktop/featureReviews/products2.csv' 
+INTO TABLE products 
+FIELDS TERMINATED BY ',' 
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n';
+
+LOAD DATA LOCAL INFILE '/Users/ishhr/Desktop/featureReviews/comments.csv' 
+INTO TABLE comments 
+FIELDS TERMINATED BY ',' 
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n';
+
+LOAD DATA LOCAL INFILE '/Users/ishhr/Desktop/featureReviews/comments2.csv' 
+INTO TABLE comments 
+FIELDS TERMINATED BY ',' 
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n';
--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
-// const db = require('../db/index.js');
+const generateData = require('./generateData.js');
+const dbMethods = require('../db/dbMethods.js');
 
 const app = express();
 const port = 4030;
@@ -9,26 +10,40 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static('public/dist'));
 
-app.get('/comments', (req, res) => {
-  db.fetchAllComments()
-    .then(output => {
-      output = JSON.stringify(output);
-      res.end(output);
-    })
-    .catch(() => {
-      res.status(400).end();
+app.get('/seed', (req, res) => {
+  for (let i = 0; i < 1000000; i++) {
+    let values = generateData.productsData();
+    dbMethods.addProductsToDB(values, (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(result);
+      }
     });
+  }
+  res.end('Thanks');
 });
 
-app.get('/comments/:id', (req, res) => {
-  db.fetchSpecificComment(req.params.id)
-    .then(data => {
-      res.send(data);
-    })
-    .catch(err => {
-      res.status(400).end();
-    });
-});
+// app.get('/comments', (req, res) => {
+//   db.fetchAllComments()
+//     .then(output => {
+//       output = JSON.stringify(output);
+//       res.end(output);
+//     })
+//     .catch(() => {
+//       res.status(400).end();
+//     });
+// });
+
+// app.get('/comments/:id', (req, res) => {
+//   db.fetchSpecificComment(req.params.id)
+//     .then(data => {
+//       res.send(data);
+//     })
+//     .catch(err => {
+//       res.status(400).end();
+//     });
+// });
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
